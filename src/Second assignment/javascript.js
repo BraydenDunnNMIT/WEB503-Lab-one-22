fe811e2f73fa14a9609e4916aad34a9b5ab1303c
Brayden Dunn <Brayden-Dunn@live.nmit.ac.nz>
2022-08-16 12:49:34 +1200

    half done

--- src/Second assignment/javascript.js
+++ src/Second assignment/javascript.js
@@ -9,7 +9,7 @@ var array= [1,2,3,4];
 return array[0];
 console.log(0);
 //returns first numb in array
-*/
+
 //creates a function that takes an array of objects, which will calculate the total price of a built computer
 
 var array1=[{
@@ -54,4 +54,44 @@ var array1=[{
  }
  
  console.log(pricecalc(array1));
+*/
+//create a function that takes an array and returns an array with strings that have digits. 
+//example: arr([1a, "abc", Ali ])
+
+var array1=[{
+    digit:0,
+    name:'string0'
+ },
+ {
+     digit:1,
+     name:'string1'
+ },
+ {
+     digit:2,
+     name:'string2'
+ },
+ {
+    digit:3,
+    name:'string3'
+
+ },
+ {
+    digit4:4,
+    name:'string4'
+
+ }
+ ]
+
+ function digits(objs){
+     var digittotal = 0
+     var name = " "
+     if(name =='' ) {
+        name += obj.name
+    }
+    objs.forEach(obj => {
+        digittotal += obj.digits
+    })
+    return 'Total digits = ' + digittotal + name 
+ }
+ console.log(digits(array1))
 
